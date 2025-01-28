2854110159e642397e5557b84ceb377b29c82f1b
Jonathan Raphaelson <jon@accidental.cc>
2025-01-28 16:44:47 -0700

    number field can show a suffix

--- app/components/ui/number-field/NumberFieldInput.vue
+++ app/components/ui/number-field/NumberFieldInput.vue
@@ -4,17 +4,21 @@ import { NumberFieldInput } from 'radix-vue';
 
 const props = defineProps({
   class: { type: null, required: false },
+  suffix: { type: String, required: false },
 });
 </script>
 
 <template>
-  <NumberFieldInput
-    data-slot="input"
-    :class="
-      cn(
-        'flex h-9 w-full rounded-sm border border-input bg-transparent py-1 text-sm text-center shadow-sm transition-colors placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
-        props.class,
-      )
-    "
-  />
+  <div class="transition-colors has-[:disabled]:opacity-50">
+    <NumberFieldInput
+      data-slot="input"
+      :class="
+        cn(
+          'flex h-9 w-full rounded-sm border border-input bg-transparent py-1 pt-0.5 text-sm text-center shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed ',
+          props.class,
+       )
+      "
+    />
+    <span v-if="props.suffix" class="absolute right-9 top-1/2 -translate-y-1/2 text-xs text-muted-foreground pointer-events-none">{{props.suffix}}</span>
+  </div>
 </template>
